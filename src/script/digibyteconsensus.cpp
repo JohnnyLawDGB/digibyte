885079a146167612ecf80026e868675be6b9e721
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2025-07-10 22:24:08 -0600

    DGB v8.22 merge with BTC v26.2 Fixes Part 23

--- src/script/digibyteconsensus.cpp
+++ src/script/digibyteconsensus.cpp
@@ -9,6 +9,7 @@
 #include <primitives/transaction.h>
 #include <pubkey.h>
 #include <script/interpreter.h>
+#include <span.h>
 #include <version.h>
 
 namespace {
@@ -23,22 +24,6 @@ public:
     m_remaining(txToLen)
     {}
 
-<<<<<<< HEAD
-    void read(char* pch, size_t nSize)
-    {
-        if (nSize > m_remaining)
-            throw std::ios_base::failure(std::string(__func__) + ": end of data");
-
-        if (pch == nullptr)
-            throw std::ios_base::failure(std::string(__func__) + ": bad destination buffer");
-
-        if (m_data == nullptr)
-            throw std::ios_base::failure(std::string(__func__) + ": bad source buffer");
-
-        memcpy(pch, m_data, nSize);
-        m_remaining -= nSize;
-        m_data += nSize;
-=======
     void read(Span<std::byte> dst)
     {
         if (dst.size() > m_remaining) {
@@ -56,7 +41,6 @@ public:
         memcpy(dst.data(), m_data, dst.size());
         m_remaining -= dst.size();
         m_data += dst.size();
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
     }
 
     template<typename T>
@@ -80,15 +64,6 @@ inline int set_error(digibyteconsensus_error* ret, digibyteconsensus_error serro
     return 0;
 }
 
-<<<<<<< HEAD
-struct ECCryptoClosure
-{
-    ECCVerifyHandle handle;
-};
-
-ECCryptoClosure instance_of_eccryptoclosure;
-=======
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 } // namespace
 
 /** Check that all specified flags are part of the libconsensus interface. */
@@ -99,20 +74,12 @@ static bool verify_flags(unsigned int flags)
 
 static int verify_script(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, CAmount amount,
                                     const unsigned char *txTo        , unsigned int txToLen,
-<<<<<<< HEAD
-=======
                                     const UTXO *spentOutputs, unsigned int spentOutputsLen,
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
                                     unsigned int nIn, unsigned int flags, digibyteconsensus_error* err)
 {
     if (!verify_flags(flags)) {
         return set_error(err, digibyteconsensus_ERR_INVALID_FLAGS);
     }
-<<<<<<< HEAD
-    try {
-        TxInputStream stream(PROTOCOL_VERSION, txTo, txToLen);
-        CTransaction tx(deserialize, stream);
-=======
 
     if (flags & digibyteconsensus_SCRIPT_FLAGS_VERIFY_TAPROOT && spentOutputs == nullptr) {
         return set_error(err, digibyteconsensus_ERR_SPENT_OUTPUTS_REQUIRED);
@@ -135,7 +102,6 @@ static int verify_script(const unsigned char *scriptPubKey, unsigned int scriptP
             }
         }
 
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
         if (nIn >= tx.vin.size())
             return set_error(err, digibyteconsensus_ERR_TX_INDEX);
         if (GetSerializeSize(tx, PROTOCOL_VERSION) != txToLen)
@@ -145,22 +111,17 @@ static int verify_script(const unsigned char *scriptPubKey, unsigned int scriptP
         set_error(err, digibyteconsensus_ERR_OK);
 
         PrecomputedTransactionData txdata(tx);
-<<<<<<< HEAD
-=======
 
         if (spentOutputs != nullptr && flags & digibyteconsensus_SCRIPT_FLAGS_VERIFY_TAPROOT) {
             txdata.Init(tx, std::move(spent_outputs));
         }
 
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
         return VerifyScript(tx.vin[nIn].scriptSig, CScript(scriptPubKey, scriptPubKey + scriptPubKeyLen), &tx.vin[nIn].scriptWitness, flags, TransactionSignatureChecker(&tx, nIn, amount, txdata, MissingDataBehavior::FAIL), nullptr);
     } catch (const std::exception&) {
         return set_error(err, digibyteconsensus_ERR_TX_DESERIALIZE); // Error deserializing
     }
 }
 
-<<<<<<< HEAD
-=======
 int digibyteconsensus_verify_script_with_spent_outputs(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, int64_t amount,
                                     const unsigned char *txTo        , unsigned int txToLen,
                                     const UTXO *spentOutputs, unsigned int spentOutputsLen,
@@ -170,19 +131,14 @@ int digibyteconsensus_verify_script_with_spent_outputs(const unsigned char *scri
     return ::verify_script(scriptPubKey, scriptPubKeyLen, am, txTo, txToLen, spentOutputs, spentOutputsLen, nIn, flags, err);
 }
 
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 int digibyteconsensus_verify_script_with_amount(const unsigned char *scriptPubKey, unsigned int scriptPubKeyLen, int64_t amount,
                                     const unsigned char *txTo        , unsigned int txToLen,
                                     unsigned int nIn, unsigned int flags, digibyteconsensus_error* err)
 {
     CAmount am(amount);
-<<<<<<< HEAD
-    return ::verify_script(scriptPubKey, scriptPubKeyLen, am, txTo, txToLen, nIn, flags, err);
-=======
     UTXO *spentOutputs = nullptr;
     unsigned int spentOutputsLen = 0;
     return ::verify_script(scriptPubKey, scriptPubKeyLen, am, txTo, txToLen, spentOutputs, spentOutputsLen, nIn, flags, err);
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 }
 
 
@@ -195,13 +151,9 @@ int digibyteconsensus_verify_script(const unsigned char *scriptPubKey, unsigned 
     }
 
     CAmount am(0);
-<<<<<<< HEAD
-    return ::verify_script(scriptPubKey, scriptPubKeyLen, am, txTo, txToLen, nIn, flags, err);
-=======
     UTXO *spentOutputs = nullptr;
     unsigned int spentOutputsLen = 0;
     return ::verify_script(scriptPubKey, scriptPubKeyLen, am, txTo, txToLen, spentOutputs, spentOutputsLen, nIn, flags, err);
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 }
 
 unsigned int digibyteconsensus_version()
--- src/script/interpreter.h
+++ src/script/interpreter.h
@@ -141,13 +141,10 @@ enum : uint32_t {
 
     // Making unknown public key versions (in BIP 342 scripts) non-standard
     SCRIPT_VERIFY_DISCOURAGE_UPGRADABLE_PUBKEYTYPE = (1U << 20),
-<<<<<<< HEAD
-=======
 
     // Constants to point to the highest flag in use. Add new flags above this line.
     //
     SCRIPT_VERIFY_END_MARKER
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 };
 
 bool CheckSignatureEncoding(const std::vector<unsigned char> &vchSig, unsigned int flags, ScriptError* serror);
@@ -155,11 +152,7 @@ bool CheckSignatureEncoding(const std::vector<unsigned char> &vchSig, unsigned i
 struct PrecomputedTransactionData
 {
     // BIP341 precomputed data.
-<<<<<<< HEAD
-    // These are single-SHA256, see https://github.com/digibyte/bips/blob/master/bip-0341.mediawiki#cite_note-15.
-=======
     // These are single-SHA256, see https://github.com/digibyte/bips/blob/master/bip-0341.mediawiki#cite_note-16.
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
     uint256 m_prevouts_single_hash;
     uint256 m_sequences_single_hash;
     uint256 m_outputs_single_hash;
@@ -179,8 +172,6 @@ struct PrecomputedTransactionData
 
     PrecomputedTransactionData() = default;
 
-<<<<<<< HEAD
-=======
     /** Initialize this PrecomputedTransactionData with transaction data.
      *
      * @param[in]   tx             The transaction for which data is being precomputed.
@@ -188,7 +179,6 @@ struct PrecomputedTransactionData
      * @param[in]   force          Whether to precompute data for all optional features,
      *                             regardless of what is in the inputs (used at signing
      *                             time, when the inputs aren't filled in yet). */
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
     template <class T>
     void Init(const T& tx, std::vector<CTxOut>&& spent_outputs, bool force = false);
 
@@ -227,12 +217,9 @@ struct ScriptExecutionData
     bool m_validation_weight_left_init = false;
     //! How much validation weight is left (decremented for every successful non-empty signature check).
     int64_t m_validation_weight_left;
-<<<<<<< HEAD
-=======
 
     //! The hash of the corresponding output
     std::optional<uint256> m_output_hash;
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 };
 
 /** Signature hash sizes */
@@ -247,14 +234,9 @@ static constexpr size_t TAPROOT_CONTROL_NODE_SIZE = 32;
 static constexpr size_t TAPROOT_CONTROL_MAX_NODE_COUNT = 128;
 static constexpr size_t TAPROOT_CONTROL_MAX_SIZE = TAPROOT_CONTROL_BASE_SIZE + TAPROOT_CONTROL_NODE_SIZE * TAPROOT_CONTROL_MAX_NODE_COUNT;
 
-<<<<<<< HEAD
-extern const CHashWriter HASHER_TAPLEAF;    //!< Hasher with tag "TapLeaf" pre-fed to it.
-extern const CHashWriter HASHER_TAPBRANCH;  //!< Hasher with tag "TapBranch" pre-fed to it.
-=======
 extern const HashWriter HASHER_TAPSIGHASH; //!< Hasher with tag "TapSighash" pre-fed to it.
 extern const HashWriter HASHER_TAPLEAF;    //!< Hasher with tag "TapLeaf" pre-fed to it.
 extern const HashWriter HASHER_TAPBRANCH;  //!< Hasher with tag "TapBranch" pre-fed to it.
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 
 template <class T>
 uint256 SignatureHash(const CScript& scriptCode, const T& txTo, unsigned int nIn, int nHashType, const CAmount& amount, SigVersion sigversion, const PrecomputedTransactionData* cache = nullptr);
@@ -267,11 +249,7 @@ public:
         return false;
     }
 
-<<<<<<< HEAD
-    virtual bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, const ScriptExecutionData& execdata, ScriptError* serror = nullptr) const
-=======
     virtual bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, ScriptExecutionData& execdata, ScriptError* serror = nullptr) const
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
     {
         return false;
     }
@@ -299,11 +277,7 @@ enum class MissingDataBehavior
 };
 
 template<typename T>
-<<<<<<< HEAD
-bool SignatureHashSchnorr(uint256& hash_out, const ScriptExecutionData& execdata, const T& tx_to, uint32_t in_pos, uint8_t hash_type, SigVersion sigversion, const PrecomputedTransactionData& cache, MissingDataBehavior mdb);
-=======
 bool SignatureHashSchnorr(uint256& hash_out, ScriptExecutionData& execdata, const T& tx_to, uint32_t in_pos, uint8_t hash_type, SigVersion sigversion, const PrecomputedTransactionData& cache, MissingDataBehavior mdb);
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 
 template <class T>
 class GenericTransactionSignatureChecker : public BaseSignatureChecker
@@ -323,11 +297,7 @@ public:
     GenericTransactionSignatureChecker(const T* txToIn, unsigned int nInIn, const CAmount& amountIn, MissingDataBehavior mdb) : txTo(txToIn), m_mdb(mdb), nIn(nInIn), amount(amountIn), txdata(nullptr) {}
     GenericTransactionSignatureChecker(const T* txToIn, unsigned int nInIn, const CAmount& amountIn, const PrecomputedTransactionData& txdataIn, MissingDataBehavior mdb) : txTo(txToIn), m_mdb(mdb), nIn(nInIn), amount(amountIn), txdata(&txdataIn) {}
     bool CheckECDSASignature(const std::vector<unsigned char>& scriptSig, const std::vector<unsigned char>& vchPubKey, const CScript& scriptCode, SigVersion sigversion) const override;
-<<<<<<< HEAD
-    bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, const ScriptExecutionData& execdata, ScriptError* serror = nullptr) const override;
-=======
     bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, ScriptExecutionData& execdata, ScriptError* serror = nullptr) const override;
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
     bool CheckLockTime(const CScriptNum& nLockTime) const override;
     bool CheckSequence(const CScriptNum& nSequence) const override;
 };
@@ -338,28 +308,17 @@ using MutableTransactionSignatureChecker = GenericTransactionSignatureChecker<CM
 class DeferringSignatureChecker : public BaseSignatureChecker
 {
 protected:
-<<<<<<< HEAD
-    BaseSignatureChecker& m_checker;
-
-public:
-    DeferringSignatureChecker(BaseSignatureChecker& checker) : m_checker(checker) {}
-=======
     const BaseSignatureChecker& m_checker;
 
 public:
     DeferringSignatureChecker(const BaseSignatureChecker& checker) : m_checker(checker) {}
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 
     bool CheckECDSASignature(const std::vector<unsigned char>& scriptSig, const std::vector<unsigned char>& vchPubKey, const CScript& scriptCode, SigVersion sigversion) const override
     {
         return m_checker.CheckECDSASignature(scriptSig, vchPubKey, scriptCode, sigversion);
     }
 
-<<<<<<< HEAD
-    bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, const ScriptExecutionData& execdata, ScriptError* serror = nullptr) const override
-=======
     bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, ScriptExecutionData& execdata, ScriptError* serror = nullptr) const override
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
     {
         return m_checker.CheckSchnorrSignature(sig, pubkey, sigversion, execdata, serror);
     }
@@ -375,14 +334,10 @@ public:
 };
 
 /** Compute the BIP341 tapleaf hash from leaf version & script. */
-<<<<<<< HEAD
-uint256 ComputeTapleafHash(uint8_t leaf_version, const CScript& script);
-=======
 uint256 ComputeTapleafHash(uint8_t leaf_version, Span<const unsigned char> script);
 /** Compute the BIP341 tapbranch hash from two branches.
   * Spans must be 32 bytes each. */
 uint256 ComputeTapbranchHash(Span<const unsigned char> a, Span<const unsigned char> b);
->>>>>>> bitcoin-v26-2-converted/digibyte-v26.2-naming-conversion
 /** Compute the BIP341 taproot script tree Merkle root from control block and leaf hash.
  *  Requires control block to have valid length (33 + k*32, with k in {0,1,..,128}). */
 uint256 ComputeTaprootMerkleRoot(Span<const unsigned char> control, const uint256& tapleaf_hash);

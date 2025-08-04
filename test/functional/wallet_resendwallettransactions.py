41dd0daf263a8291940943f1d4f8951057ae3768
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2025-08-04 14:03:48 -0600

    Update wallet_resendwallettransactions.py

--- test/functional/wallet_resendwallettransactions.py
+++ test/functional/wallet_resendwallettransactions.py
@@ -27,6 +27,7 @@ class ResendWalletTransactionsTest(DigiByteTestFramework):
 
     def set_test_params(self):
         self.num_nodes = 1
+        self.extra_args = [["-whitelist=noban@127.0.0.1", "-dandelion=0"]]
 
     def skip_test_if_missing_module(self):
         self.skip_if_no_wallet()

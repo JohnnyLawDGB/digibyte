39978440d54342da96fed12af023a01782fd69ae
Jared Tate <13957390+JaredTate@users.noreply.github.com>
2025-08-26 11:27:45 -0600

    Update wallet_orphanedreward.py

--- test/functional/wallet_orphanedreward.py
+++ test/functional/wallet_orphanedreward.py
@@ -16,8 +16,8 @@ class OrphanedBlockRewardTest(DigiByteTestFramework):
         self.setup_clean_chain = True
         self.num_nodes = 2
         self.extra_args = [
-            ["-dandelion=0"], 
-            ["-dandelion=0"]
+            ["-dandelion=0", "-easypow"], 
+            ["-dandelion=0", "-easypow"]
         ]
 
     def skip_test_if_missing_module(self):

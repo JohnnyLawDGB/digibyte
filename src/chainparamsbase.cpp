e908914e663cfa9bc2c04eda3e840d04e19b2fd7
DigiSwarm <245904857+DigiSwarm@users.noreply.github.com>
2025-12-09 16:37:42 -0700

    testnet8 dir, port 12030

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -43,7 +43,7 @@ std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const ChainType chain)
     case ChainType::MAIN:
         return std::make_unique<CBaseChainParams>("", 14022, 14122);
     case ChainType::TESTNET:
-        return std::make_unique<CBaseChainParams>("testnet7", 14024, 14123);
+        return std::make_unique<CBaseChainParams>("testnet8", 14025, 14124);
     case ChainType::SIGNET:
         return std::make_unique<CBaseChainParams>("signet", 19443, 19445);
     case ChainType::REGTEST:

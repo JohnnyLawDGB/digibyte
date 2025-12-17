ac3fa930da8e479a0bc8a6dc7a0c2679214cbe86
DigiSwarm <13957390+JaredTate@users.noreply.github.com>
2025-12-17 13:18:35 -0700

    Testnet Diff Adjsutment

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -384,9 +384,9 @@ public:
         consensus.initialTarget[ALGO_ODO] = ArithToUint256(~arith_uint256(0) >> 20); // Same as other algos
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
         consensus.nPowTargetSpacing = 60 / 4;
-        consensus.fPowAllowMinDifficultyBlocks = true;
+        consensus.fPowAllowMinDifficultyBlocks = false; // Disable min difficulty blocks for proper difficulty adjustment
         consensus.fEasyPow = false;
-        consensus.fPowNoRetargeting = false;
+        consensus.fPowNoRetargeting = false; // Enable difficulty retargeting
         consensus.nRuleChangeActivationThreshold = 4032; // 4032 - 70% of 5760
         consensus.nMinerConfirmationWindow = 5760; // 1 day of blocks on testnet
         consensus.fRbfEnabled = false;

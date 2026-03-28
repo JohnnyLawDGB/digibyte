3e9087dc09ed9f1575ed16fe3ed7efd5e9e98b1f
DigiSwarm <13957390+JaredTate@users.noreply.github.com>
2026-03-27 19:38:01 -0600

    fuzz: fix tx_pool SUPPLY_TOTAL for DGB Period I reward (72000 COIN)
    Regtest mines blocks at heights < 1440, which falls in DigiByte's
    Period I reward schedule (72000 DGB/block). The previous value of
    8000 (Period III) caused assertion failures in the supply tracking
    check because actual mined supply was 9x higher than expected.

--- src/test/fuzz/tx_pool.cpp
+++ src/test/fuzz/tx_pool.cpp
@@ -149,8 +149,8 @@ FUZZ_TARGET(tx_pool_standard, .init = initialize_tx_pool)
     outpoints_rbf = outpoints_supply;
 
     // The sum of the values of all spendable outpoints
-    // DigiByte: Using 8000 DGB per block (Period III reward)
-    constexpr CAmount SUPPLY_TOTAL{COINBASE_MATURITY * 8000 * COIN};
+    // DigiByte: Regtest mines at heights < 1440, which is Period I (72000 DGB/block)
+    constexpr CAmount SUPPLY_TOTAL{COINBASE_MATURITY * 72000 * COIN};
 
     SetMempoolConstraints(*node.args, fuzzed_data_provider);
     CTxMemPool tx_pool_{MakeMempool(fuzzed_data_provider, node)};

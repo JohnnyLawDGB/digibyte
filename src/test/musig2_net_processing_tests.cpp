7e4e314b7151777b9df438b5a9767d5ca1905d7d
DigiSwarm <13957390+JaredTate@users.noreply.github.com>
2026-03-29 14:39:37 -0600

    test: add MuSig2 net processing message coverage

---
+++ src/test/musig2_net_processing_tests.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2024-2026 The DigiByte Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <boost/test/unit_test.hpp>
+
+#include <protocol.h>
+#include <test/util/setup_common.h>
+
+#include <algorithm>
+
+BOOST_FIXTURE_TEST_SUITE(musig2_net_processing_tests, BasicTestingSetup)
+
+BOOST_AUTO_TEST_CASE(musig2_netmsg_types_are_registered)
+{
+    const auto& msg_types = getAllNetMessageTypes();
+
+    BOOST_CHECK(std::find(msg_types.begin(), msg_types.end(), NetMsgType::ORACLEMUSIGNONCE) != msg_types.end());
+    BOOST_CHECK(std::find(msg_types.begin(), msg_types.end(), NetMsgType::ORACLEMUSIGPARTIALSIG) != msg_types.end());
+}
+
+BOOST_AUTO_TEST_CASE(musig2_inv_types_map_to_wire_commands)
+{
+    const CInv inv_nonce{MSG_ORACLE_MUSIG_NONCE, uint256::ZERO};
+    BOOST_CHECK(inv_nonce.IsOracleMsg());
+    BOOST_CHECK_EQUAL(inv_nonce.GetCommand(), NetMsgType::ORACLEMUSIGNONCE);
+
+    const CInv inv_psig{MSG_ORACLE_MUSIG_PARTIALSIG, uint256::ZERO};
+    BOOST_CHECK(inv_psig.IsOracleMsg());
+    BOOST_CHECK_EQUAL(inv_psig.GetCommand(), NetMsgType::ORACLEMUSIGPARTIALSIG);
+}
+
+BOOST_AUTO_TEST_CASE(musig2_messages_validate_expected_field_lengths)
+{
+    OracleMusigNonceMsg nonce_msg;
+    nonce_msg.epoch = 123;
+    nonce_msg.oracle_id = 1;
+    nonce_msg.pubnonce.assign(66, 0x22);
+    BOOST_CHECK(nonce_msg.IsValid());
+
+    nonce_msg.pubnonce.assign(65, 0x22);
+    BOOST_CHECK(!nonce_msg.IsValid());
+
+    OracleMusigPartialSigMsg psig_msg;
+    psig_msg.epoch = 123;
+    psig_msg.oracle_id = 1;
+    psig_msg.partial_sig.assign(32, 0x33);
+    BOOST_CHECK(psig_msg.IsValid());
+
+    psig_msg.partial_sig.assign(31, 0x33);
+    BOOST_CHECK(!psig_msg.IsValid());
+}
+
+BOOST_AUTO_TEST_SUITE_END()

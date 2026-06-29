758227c8d537cf3b65087c34398ba0a286ee81eb
DigiSwarm <13957390+JaredTate@users.noreply.github.com>
2026-06-29 09:31:34 -0600

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -4798,6 +4798,14 @@ static bool ContextualCheckBlockHeader(const CBlockHeader& block, BlockValidatio
     // Check proof of work
     const Consensus::Params& consensusParams = chainman.GetConsensus();
     int algo = block.GetAlgo();
+
+    if (DeploymentActiveAfter(pindexPrev, chainman, Consensus::DEPLOYMENT_DIGIDOLLAR)) {
+        if (algo == ALGO_UNKNOWN || !IsAlgoActive(pindexPrev, consensusParams, algo)) {
+            return state.Invalid(BlockValidationResult::BLOCK_INVALID_ALGO, "bad-algo",
+                                 "block uses a deactivated mining algorithm");
+        }
+    }
+
     unsigned int nBitsExpected = GetNextWorkRequired(pindexPrev, &block, consensusParams, algo);
     
     // Debug logging for early blocks
